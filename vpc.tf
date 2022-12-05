08911b7f32ed00edb500b4493b5bf2e1bba79ccc
Nani433 <venkatasiva8196@gmail.com>
2022-12-05 17:45:46 +0530

    changes

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region  = "ap-south-1"
-  acess_key = "AKIAU3ZWQXHCL4UVILXW"
-  secret_key = "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"
+  acess_key = "AKIAU3ZWQXHCEMZMSNG4"
+  secret_key = "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"
 }
 
 resource "aws_vpc" "vpc" {
