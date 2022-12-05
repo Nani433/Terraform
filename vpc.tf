7bc8347968b3fe2277ebc8087ab937bcb9536c90
Nani433 <venkatasiva8196@gmail.com>
2022-12-05 17:58:27 +0530

    changes

--- vpc.tf
+++ vpc.tf
@@ -1,119 +1,18 @@
-#provider "aws" {
- # region  = "ap-south-1"
-  #acess_key = "AKIAU3ZWQXHCEMZMSNG4"
-  #secret_key = "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"
-#}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-resource "aws_vpc" "vpc" {
-  cidr_block       = "192.168.0.0/16"
-  instance_tenancy = "default"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
 
-  tags = {
-    Name = "demo-vpc"
-  }
-}
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-resource "aws_subnet" "pub" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "192.168.1.0/24"
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
   tags = {
-    Name = "pubilc"
-  }
-}
-
-resource "aws_subnet" "pri" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "192.168.3.0/24"
-
-  tags = {
-    Name = "private"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-
-  tags = {
-    Name = "IGW"
-  }
-}
-
-resource "aws_eip" "ip" {
-    vpc      = true
-}
-
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.ip.id
-  subnet_id     = aws_subnet.pri.id
-
-  tags = {
-    Name = "NGW"
-  }
-}
-
-
-resource "aws_route_table" "rt1" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  
-  tags = {
-    Name = "custom"
-  }
-}
-
-resource "aws_route_table" "rt2" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.ngw.id
-  }
-
-  
-  tags = {
-    Name = "main"
-  }
-}
-
-resource "aws_route_table_association" "as_1" {
-  subnet_id      = aws_subnet.pub.id
-  route_table_id = aws_route_table.rt1.id
-}
-
-resource "aws_route_table_association" "as_2" {
-  subnet_id      = aws_subnet.pri.id
-  route_table_id = aws_route_table.rt2.id
-}
-
-resource "aws_security_group" "sg" {
-  name        = "first-SG"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.vpc.id
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22 
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.vpc.cidr_block]
-  
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "first-SG"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
